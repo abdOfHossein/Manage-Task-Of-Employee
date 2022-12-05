adf0dbb8e5af325e3f5c55619216b7f2f311597d
abdOfHossein <92151678+abdOfHossein@users.noreply.github.com>
2022-12-05 10:56:23 +0330

    p2

--- src/modules/user/user-core/controller/user.controller.ts
+++ src/modules/user/user-core/controller/user.controller.ts
@@ -5,6 +5,7 @@ import {
   Post,
   Put,
   Query,
+  Res,
   Req,
   UseGuards,
 } from '@nestjs/common';
@@ -18,7 +19,7 @@ import { UserEnt } from '../../modules/entities/User.entity';
 import { RolesGuard } from '../../modules/guard/role.guard';
 import { UserPageDto } from '../../modules/paginations/user.page.dto';
 import { UserService } from '../../modules/services/User.service';
-
+import{Response} from 'express'
 @ApiTags('User')
 @Controller('User')
 export class UserController {
@@ -40,8 +41,10 @@ export class UserController {
   }
   //login
   @Post('login')
-  login(@Body() loginUserDto: LoginUserDto): Promise<Object> {
-    return this.user._createJwt(loginUserDto);
+ async login(@Body() loginUserDto: LoginUserDto , @Res() res:Response ){
+    const result= await this.user._createJwt(loginUserDto);
+    res.status(200).send(result)
+
   }
 
   @UseGuards(RolesGuard)
